6f1fba6a054c46b2a8abb9a48c3b9882b6f2c3ae
Hill <hill@kimo.com>
2016-02-23 05:20:36 +0000

    fixed laze output

--- view_cli.php
+++ view_cli.php
@@ -7,6 +7,9 @@ class view_cli extends ViewEngine
 {
     public function process()
     {
+        if (!empty($this['forward']->action)) {
+            return;
+        }
         $all = $this->get();
         \PMVC\plug('cli')->dump($all,'%C');
     }
